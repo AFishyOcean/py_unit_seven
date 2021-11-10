e6e0d0d11894ab8a3cae22270755fc6e166651e5
AFishyOcean <dljhazelh@gmail.com>
2021-11-10 10:59:28 -0500

    worked on strings

--- strings_one.py
+++ strings_one.py
@@ -1,10 +1,13 @@
 def half_slice(word):
+    x = len(word) // 2
+    word2 = word[x:] + word[:x]
+    return word2
 
 
-
-def no_first_last(str):
-    pass
-
+def no_first_last(word):
+    word = word.replace(word[0], '')
+    word = word.replace(word[-1], '')
+    return word
 
 def longest(phrase):
     pass
@@ -16,9 +19,9 @@ def title_case(sentence):
 def main():
     word = 'Something'
     print(half_slice(word))
-    print(no_first_last(str))
-    print(longest(phrase))
-    print(title_case(sentence))
+    print(no_first_last(word))
+    # print(longest(phrase))
+    # print(title_case(sentence))
 
 if __name__ == '__main__':
     main()
