79b8eff72e3f85415c96e14ba0a4530c97057f5d
AFishyOcean <dljhazelh@gmail.com>
2021-11-12 14:33:05 -0500

    worked on strings

---
+++ assignment_seven.py
@@ -0,0 +1,21 @@
+def questions():
+    input1 = str(input("Enter sentence:"))
+    input2 = int(input("What is the shift:"))
+    input2 = input2 % 25
+    input3 = input("Are you encoding(y/n)")
+    inputs = [input1, input2, input3]
+    return inputs
+
+def coding():
+    pass
+
+
+def main():
+    answers = questions()
+    x = answers[0]
+    y = answers[1]
+    print(x)
+    print(y)
+
+if __name__ == '__main__':
+    main()
