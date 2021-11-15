c789f16142112fd1c60375997be233c20e95687f
AFishyOcean <dljhazelh@gmail.com>
2021-11-15 14:02:42 -0500

    worked on project 7

--- assignment_seven.py
+++ assignment_seven.py
@@ -1,23 +1,34 @@
 def questions():
     input1 = str(input("Enter sentence:"))
     input2 = int(input("What is the shift:"))
-    input2 = input2 % 25
     input3 = input("Are you encoding(y/n)")
     if input3 == "n":
         input2 = -input2
-    inputs = [input1, input2]
+    inputs = [input1, input2, input3]
     return inputs
 
-def coding():
-    pass
-
+def coding(answers):
+    alphBET = 'abcdefghijklmnopqrstuvwxyz'
+    sentence = answers[0]
+    shift = answers[1]
+    coded_sentence = ' '
+    for x in range(len(alphBET)):
+        letter = sentence[x]
+        if letter in alphBET:
+            value = alphBET.index(letter)
+            value_shifted = value + shift
+            valueSIZEDshifted = value_shifted % 26
+            new_letter = alphBET[valueSIZEDshifted]
+        coded_sentence = coded_sentence + new_letter
+    return coded_sentence
 
 def main():
     answers = questions()
-    x = answers[0]
-    y = answers[1]
-    print(x)
-    print(y)
+    if answers[2] == "y":
+        print("Your encoded message is,", coding(answers))
+    else:
+        print("Your decoded message is,", coding(answers))
+
 
 if __name__ == '__main__':
     main()
